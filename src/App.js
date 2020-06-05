9974efe5142b63fac8ee937491b03b8fc96baf02
yonny <yonny.nova@ixev.com>
2020-06-04 22:09:25 -0500

    adding styled button component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
 import Person from './Person/Person'
 
+
+const StyledButton = styled.button`
+  background-color: green;
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: 'pointer';
+
+  &:hover {
+    background-color: lightgreen;
+    color:black;
+  }
+`;
 
 class App extends Component {
   
@@ -50,18 +64,7 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover':{
-        backgroundColor: 'lightgreen',
-        color:'black'
-      }
-    };
+
 
     let persons = null;
 
@@ -83,13 +86,6 @@ class App extends Component {
             </div>
        );
        
-        style.backgroundColor= 'red';
-
-        style[':hover']={
-          backgroundColor: 'salmon',
-          color:'black'
-        }
-
       }
 
       const classes = [];
@@ -107,8 +103,8 @@ class App extends Component {
         <div className="App">
           <h1>hi I'm a react app</h1>
           <p className={classes.join(' ')} >This is working from Javascript and react !!!!!</p>
-          <button style={style} 
-                  onClick={this.togglePersonsHandler}> Toggle Persons </button>
+          <StyledButton 
+                  onClick={this.togglePersonsHandler}> Toggle Persons </StyledButton>
           {persons}
 
         </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -20,21 +20,14 @@ const StyleDiv =  styled.div`
 
 
 const person = (props) => {
-    const style = {
-        '@media (min-width : 500px)':{
-            width: '450px'
-        }
-    };
+    
     return (
-
-
         // <div className="Person" style={style}>
         <StyleDiv>
             <p onClick= {props.click} >I'm a {props.name} and I am {props.age} years old! </p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
         </StyleDiv>
-
         // </div>
     )
 }
